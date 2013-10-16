3f159ad7db09e8dfa0ca97cde03bf83024b59890
Jeff Beck <jeff.beck@reachlocal.com>
2013-10-16 15:47:40 -0500

    Bump version number

--- DatabaseMigrationGrailsPlugin.groovy
+++ DatabaseMigrationGrailsPlugin.groovy
@@ -41,7 +41,7 @@ import liquibase.sqlgenerator.core.CreateTableGenerator
 
 class DatabaseMigrationGrailsPlugin {
 
-	String version = '1.3.6'
+	String version = '1.3.7-SNAPSHOT'
 	String grailsVersion = '2.0 > *'
 	String author = 'Burt Beckwith'
 	String authorEmail = 'bbeckwith@gopivotal.com'
